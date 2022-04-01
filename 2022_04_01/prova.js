db73a897282de3b287db6b90ccc943e8b12ededd
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2022-04-01 19:01:43 +0200

    lo dovevo far prima

---
+++ 2022_04_01/prova.js
@@ -0,0 +1,21 @@
+$('.benvenuto').css('color','green')
+//$('#string').text()
+//console.log($('#string').val())
+
+
+$("#button").click(function(){
+   let str = $("#string").val()
+   //alert(str)
+   $("p:contains(str)").css("background-color","#FFFF00")
+})
+
+$("p:last")
+  .detach()
+  .insertBefore("p:eq(2)")
+
+  
+
+$( "#button2" ).trigger( "click" ) //triggero il click e metto los fondo
+$( "#button2" ).on("click", function() {
+    $( this ).css("background-color","RED")
+  });
