14a0385fe8d156e1ce251c2cffdf4bf447a6397f
enrico giorgi <enrico.giorgi8@studio.unibo.it>
2022-04-03 10:43:36 +0200

    modifiche 0.0.2

--- 2022_03_31/mytest3.php
+++ 2022_03_31/mytest3.php
@@ -7,7 +7,7 @@ if ($conn->connect_errno) {
     die("Connessione in errore: " . $conn->connect_error); 
 } 
 //$data = $_GET["id"];
-$sql="SELECT * FROM utenti";
+$sql="SELECT * FROM allievi";
 $stmt=$conn->prepare($sql);
 //$stmt->bind_param("i",$_GET["id"]); 
 $stmt->execute();
@@ -16,6 +16,7 @@ $data=[];
 while($f=$result->fetch_assoc()){
     array_push($data, $f);
 }
+
 header('Content-Type: application/json'); 
 //specifico formato json 
 echo json_encode($data);
--- 2022_03_31/prova.js
+++ 2022_03_31/prova.js
@@ -1,20 +1,24 @@
 //sta creando un oggetto ->{} non e una funzione
 //     function vuota
-  
+ 
 function utente(nome, cognome) {
-    let u = Object.create(utente.methods)
+  let u = Object.create(utente.methods)
     u.nome = nome
-    u.cognome=cognome
-    return u }
+    u.cognome= cognome
+    console.log(u.toString())
+    return u 
+  }
   utente.methods = { 
-          toString: function() { return "Utente: " + this.nome +" "+this.cognome } , country:"Italy"       
+          toString: function() 
+          { return "Utente: " + this.nome +" "+this.cognome }
+           ,country:"Italy"       
   };
-  let a = utente("Andrea","Cesanelli")
-  console.log(a.toString())
-  console.log(a)
-  a.country
   
-
+  let a = utente("Andrea","Cesanelli")
+ // console.log(a.toString())
+  console.log(a)
+  console.log(a.country)
+  
 //utente.methods = { 
 //  toString() { return "Utente: " + this.nome }         
 //};
